00dd6b67e1a906dede79b7bf8eeecbd1c979264d
thgs <theogpl57@gmail.com>
2018-10-21 01:25:35 +0300

    added default admin user in migrations

--- database/migrations/2018_10_01_000000_create_users_table.php
+++ database/migrations/2018_10_01_000000_create_users_table.php
@@ -24,6 +24,14 @@ class CreateUsersTable extends Migration
             $table->timestamps();
             $table->softDeletes();
         });
+
+        $array_to_insert = array(
+            array('id' => 1, 'level' => 10, 'name' => 'Demo Admin', 'email' => 'demo@example.org', 'password' => '$2y$10$sD2uIkgmJN31jfT4H0c1LeaOyIIhMzIdpvkDLtEnbHG10WfnCPEja', 'remember_token' => str_random(10))
+        );
+
+        foreach ($array_to_insert as $item){
+            DB::table('users')->insert($item);
+        }
     }
 
     /**
