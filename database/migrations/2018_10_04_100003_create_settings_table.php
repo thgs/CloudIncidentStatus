f54b12ac6c29f4798484bff363ac96da742fd10b
thgs <theogpl57@gmail.com>
2018-10-21 01:22:11 +0300

    adding default settings in migration

--- database/migrations/2018_10_04_100003_create_settings_table.php
+++ database/migrations/2018_10_04_100003_create_settings_table.php
@@ -30,6 +30,15 @@ class CreateSettingsTable extends Migration
             $table->timestamps();
             $table->softDeletes();
         });
+
+        $array_to_insert = array(
+            array('id' => 1, 'time_zone_interface' => "UTC", 'title_app' => 'Cloud VPS Hosting', 'settings_logo' => 'logo.png', 'days_of_incidents' => 7, 'bulk_emails' => 20, 'bulk_emails_sleep' => 10, 'queue_name_incidents' => 'Incidents', 'queue_name_maintenance' => 'Maintenance', 'from_address' => 'demo@example.org', 'from_name' => 'Demo Name', 'allow_subscribers' => 1 )
+        );
+
+        foreach ($array_to_insert as $item){
+            DB::table('settings')->insert($item);
+        }
+
     }
 
     /**
